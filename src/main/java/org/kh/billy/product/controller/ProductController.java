8cabe7acf9298e34d4524599a8b3a477e0c3ffae
Seokho Youn <axlover@hotmail.com>
2019-04-26 21:23:45 +0900

    20190426 up

--- src/main/java/org/kh/billy/product/controller/ProductController.java
+++ src/main/java/org/kh/billy/product/controller/ProductController.java
@@ -12,7 +12,6 @@ import org.kh.billy.product.model.vo.SettingList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
--- src/main/webapp/resources/js/productList.js
+++ src/main/webapp/resources/js/productList.js
@@ -1,129 +1,10 @@
-const products = [
-    {
-        productNo: 1,
-        name: "pizza",
-        image1: "https://images.pexels.com/photos/2147192/pexels-photo-2147192.png?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        image2: "https://images.pexels.com/photos/2128738/pexels-photo-2128738.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        price: "50,000원",
-        rating: 10
-    },
-    {
-        productNo: 2,
-        name: "computer",
-        image1: "https://images.pexels.com/photos/2123345/pexels-photo-2123345.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        image2: "https://images.pexels.com/photos/2104140/pexels-photo-2104140.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        price: "20,000원",
-        rating: 8
-    },
-    {
-        productNo: 3,
-        name: "potatoes",
-        image1: "https://images.pexels.com/photos/4316/technology-computer-chips-gigabyte.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        image2: "https://images.pexels.com/photos/2124885/pexels-photo-2124885.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        price: "40,000원",
-        rating: 9
-    },
-    {
-        productNo: 4,
-        name: "Pumkins 대여",
-        image1: "https://images.pexels.com/photos/792345/pexels-photo-792345.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        image2: "https://images.pexels.com/photos/2124885/pexels-photo-2124885.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        price: "10,000원",
-        rating: 5
-    },
-    {
-        productNo: 5,
-        name: "pizza",
-        image1: "https://images.pexels.com/photos/2147192/pexels-photo-2147192.png?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        image2: "https://images.pexels.com/photos/2128738/pexels-photo-2128738.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        price: "50,000원",
-        rating: 10
-    },
-    {
-        productNo: 6,
-        name: "computer",
-        image1: "https://images.pexels.com/photos/2123345/pexels-photo-2123345.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        image2: "https://images.pexels.com/photos/2104140/pexels-photo-2104140.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        price: "20,000원",
-        rating: 8
-    },
-    {
-        productNo: 7,
-        name: "potatoes",
-        image1: "https://images.pexels.com/photos/4316/technology-computer-chips-gigabyte.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        image2: "https://images.pexels.com/photos/2124885/pexels-photo-2124885.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        price: "40,000원",
-        rating: 9
-    },
-    {
-        productNo: 8,
-        name: "Pumkins 대여",
-        image1: "https://images.pexels.com/photos/792345/pexels-photo-792345.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        image2: "https://images.pexels.com/photos/2124885/pexels-photo-2124885.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
-        price: "10,000원",
-        rating: 5
-    }
-]
-for(let i = 0; i < products.length; i++){
-    const listRow = document.getElementById('product_list_row');
+let chosenPage = 1;
+let listCount = 12;
+let minPrice = 0;
+let maxPrice = 100000;
+let sub = [];
 
-    const gridDiv = document.createElement('div');
-    const singleProductWrapper = document.createElement('div');
-    const productImg = document.createElement("div");
-    const mainImg = document.createElement('img');
-    const hovImg = document.createElement('img');
-    const description = document.createElement('div');
-    const productMetaData = document.createElement('div');
-    const divLine = document.createElement('div');
-    const productPrice = document.createElement('p');
-    const productName = document.createElement('h6');
-    const ratingDiv = document.createElement('div');
-    const ratingBox = document.createElement('div');
-    
-    gridDiv.classList = "col-12 col-sm-6 col-md-12 col-xl-6";
-    singleProductWrapper.classList = "single-product-wrapper";
-    productImg.classList = "product-img";
-    hovImg.classList = "hover-img";
-    description.classList = "product-description d-flex align-items-center justify-content-between";
-    productMetaData.classList = "product-meta-data";
-    divLine.classList = "line";
-    productPrice.classList = "product-price";
-    ratingDiv.classList = "ratings-cart text-right";
-    ratingBox.classList = "ratings";
-    productPrice.textContent = products[i].price;
-    productName.textContent = products[i].name;
-    
-    const wrapper = listRow.appendChild(gridDiv).appendChild(singleProductWrapper);
-    const imageSection = wrapper.appendChild(productImg);
-    imageSection.appendChild(mainImg);
-    imageSection.appendChild(hovImg);
-    mainImg.setAttribute('src',products[i].image1);
-    hovImg.setAttribute('src',products[i].image2);
-    const descSection = wrapper.appendChild(description);
-    const metaDataSection = descSection.appendChild(productMetaData);
-    metaDataSection.appendChild(divLine);
-    metaDataSection.appendChild(productPrice);
-    metaDataSection.appendChild(productName);
-    const ratingSection = descSection.appendChild(ratingDiv).appendChild(ratingBox);
-    
-    const ratingIndex = products[i].rating/2;
-    console.log(ratingIndex);
-    console.log(ratingIndex%2);
-    for(let i = 0; i < ratingIndex; i++){
-        const starIcon = document.createElement('i');
-        starIcon.classList = "fa fa-star";
-        starIcon.setAttribute('aria-hidden','true');
-        ratingSection.appendChild(starIcon);
-    }
-    if(ratingIndex%2 === 0.5){
-        ratingSection.removeChild(ratingSection.firstChild)
-        const starIcon = document.createElement('i');
-        starIcon.classList = "fa fa-star-half-o";
-        starIcon.setAttribute('aria-hidden','true');
-        ratingSection.appendChild(starIcon);
-    }
-}
-
+// 서브 카테고리 변경용 자바 스크립트
 const catagoryRadios = document.querySelectorAll('.catagories-menu input[name="item"]');
 const subCatagory = document.querySelector('.widget-desc');
 for(let i = 0; i < catagoryRadios.length; i++){
@@ -131,14 +12,117 @@ for(let i = 0; i < catagoryRadios.length; i++){
         while(subCatagory.firstChild){
             subCatagory.removeChild(subCatagory.firstChild);
         }
-        const formCheck = document.createElement('div');
-        formCheck.classList = "form-check";
-        const checkbox = document.createElement('input');
-        checkbox.setAttribute('type','checkbox');
-        checkbox.classList= "form-check-input";
-
+        
         if(catagoryRadios[i].value === 'living'){
-            subCatagory.appendChild(formCheck).appendChild(checkbox);
+            const sub = [{cno:1, cname:"생활용품"},{cno:2, cname:"주방용품"},{cno:3,cname:"가구/인테리어"},{cno:4,cname:"키덜트/취미"}];
+            for(let i = 0; i < sub.length; i++){
+                const formCheck = document.createElement('div');
+                const checkbox = document.createElement('input');
+                const label = document.createElement('label');
+
+                formCheck.classList = "form-check";
+                checkbox.classList= "form-check-input";
+                label.classList = "form-check-label";
+
+                checkbox.setAttribute('type','checkbox');
+                checkbox.setAttribute('value',sub[i].cno);
+                checkbox.setAttribute('id',sub[i].cno);
+                label.setAttribute('for',sub[i].cno);
+
+                label.textContent = sub[i].cname;
+                const form = subCatagory.appendChild(formCheck);
+                form.appendChild(checkbox);
+                form.appendChild(label);
+                console.log("created!");
+            }
         }
     })
 }
+
+function requestProductListAjax(){
+    const xhr = new XMLHttpRequest();
+
+    xhr.onload = ()=>{
+        const products = JSON.parse(xhr.responseText);
+        
+        for(let i = 0; i < products.list.length; i++){
+            const listRow = document.getElementById('product_list_row');
+        
+            const gridDiv = document.createElement('div');
+            const singleProductWrapper = document.createElement('div');
+            const productImg = document.createElement("div");
+            const mainImg = document.createElement('img');
+            
+            const description = document.createElement('div');
+            const productMetaData = document.createElement('div');
+            const divLine = document.createElement('div');
+            const productPrice = document.createElement('p');
+            const productName = document.createElement('h6');
+            const ratingDiv = document.createElement('div');
+            const ratingBox = document.createElement('div');
+            
+            gridDiv.classList = "col-12 col-sm-6 col-md-12 col-xl-6";
+            singleProductWrapper.classList = "single-product-wrapper";
+            productImg.classList = "product-img";
+            
+            description.classList = "product-description d-flex align-items-center justify-content-between";
+            productMetaData.classList = "product-meta-data";
+            divLine.classList = "line";
+            productPrice.classList = "product-price";
+            ratingDiv.classList = "ratings-cart text-right";
+            ratingBox.classList = "ratings";
+            productPrice.textContent = numberWithCommas(products.list[i].price)+"원";
+            productName.textContent = products.list[i].pname;
+            
+            const wrapper = listRow.appendChild(gridDiv).appendChild(singleProductWrapper);
+            const imageSection = wrapper.appendChild(productImg);
+            imageSection.appendChild(mainImg);
+            
+            mainImg.setAttribute('src','/myapp/resources/files/product/'+products.list[i].img);
+            
+            const descSection = wrapper.appendChild(description);
+            const metaDataSection = descSection.appendChild(productMetaData);
+            metaDataSection.appendChild(divLine);
+            metaDataSection.appendChild(productPrice);
+            metaDataSection.appendChild(productName);
+            // const ratingSection = descSection.appendChild(ratingDiv).appendChild(ratingBox);
+            
+            // const ratingIndex = products[i].rating/2;
+            
+            // for(let i = 0; i < ratingIndex; i++){
+            //     const starIcon = document.createElement('i');
+            //     starIcon.classList = "fa fa-star";
+            //     starIcon.setAttribute('aria-hidden','true');
+            //     ratingSection.appendChild(starIcon);
+            // }
+            // if(ratingIndex%2 === 0.5){
+            //     ratingSection.removeChild(ratingSection.firstChild)
+            //     const starIcon = document.createElement('i');
+            //     starIcon.classList = "fa fa-star-half-o";
+            //     starIcon.setAttribute('aria-hidden','true');
+            //     ratingSection.appendChild(starIcon);
+            // }
+        }
+    }
+    
+    const requestData = {
+        pcategory_name: 12,
+        sub_pcategory_name: 23,
+        page : chosenPage,
+        listCount: listCount,
+
+
+    }
+    xhr.open('POST','getProductList.do');
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify(requestData));
+}
+
+// 숫자 세자리마다 ,찍기 함수
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+// 페이지 로드시 AJAX 실행
+document.addEventListener('DOMContentLoaded',() =>{
+    requestProductListAjax();
+})
